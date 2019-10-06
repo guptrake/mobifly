cffe471979671caeab0ebafb9691a7709652a728
guptrake <it.rakesh.gupta@gmail.com>
2019-10-06 14:22:49 -0700

    form layout_deselect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles:[ `
+  styles: [`
   .locked {pointer-events:none;}
   `]
 })
@@ -15,12 +15,12 @@ export class AppComponent implements OnInit {
   colFile2: string[];
   selectedItems = [];
   dropdownSettings = {};
-  fileArray: any[]=[];
-  keyColumnsFile1: any[]=[];
-  keyColumnsFile2: any[]=[];
-  isGroup:any=false;
-  groupColumnFile1: any[]=[];
-  groupColumnFile2: any[]=[];
+  fileArray: any[] = [];
+  keyColumnsFile1: any[] = [];
+  keyColumnsFile2: any[] = [];
+  isGroup: any = false;
+  groupColumnFile1: any[] = [];
+  groupColumnFile2: any[] = [];
 
 
 
@@ -60,26 +60,59 @@ export class AppComponent implements OnInit {
     };
   }
   onColSelectFile1(keyCol: any) {
-    this.keyColumnsFile1.push(this.colFile1.indexOf(keyCol));
+    //this.keyColumnsFile1.push(this.colFile1.indexOf(keyCol));
+    this.keyColumnsFile1.push(keyCol);
   }
   onColSelectFile2(keyCol: any) {
-    this.keyColumnsFile2.push(this.colFile2.indexOf(keyCol));
+    //this.keyColumnsFile2.push(this.colFile2.indexOf(keyCol));
+    this.keyColumnsFile2.push(keyCol)
+  }
+  removeKeyColFile1(colRemoved) {
+    //const colNumberToRemove = this.groupColumnFile1.indexOf(colRemoved) + 1;
+    this.keyColumnsFile1 = this.keyColumnsFile1.filter(item => item !== colRemoved)
+//console.log(this.keyColumnsFile1)
+  }
+  removeKeyColFile2(colRemoved) {
+    //const colNumberToRemove = this.groupColumnFile1.indexOf(colRemoved) + 1;
+    this.keyColumnsFile2 = this.keyColumnsFile2.filter(item => item !== colRemoved)
   }
   onSelectAll(items: any) {
     console.log(items);
   }
-  onColSelectFile1Group(groupCol:any){
-    this.groupColumnFile1.push(this.colFile1.indexOf(groupCol));
+
+
+  onColSelectFile1Group(groupCol: any) {
+    //this.groupColumnFile1.push(this.colFile1.indexOf(groupCol) + 1);
+    this.groupColumnFile1.push(groupCol)
   }
-  onColSelectFile2Group(groupCol:any){
-    this.groupColumnFile2.push(this.colFile2.indexOf(groupCol));
+  onColSelectFile2Group(groupCol: any) {
+    //this.groupColumnFile2.push(this.colFile2.indexOf(groupCol) + 1);
+    this.groupColumnFile2.push(groupCol);
   }
-  checkBoxEvent(data){
+  removeColFile1Group(colRemoved) {
+    //const colNumberToRemove = this.groupColumnFile1.indexOf(colRemoved) + 1;
+    this.groupColumnFile1 = this.groupColumnFile1.filter(item => item !== colRemoved)
+  }
+  removeColFile2Group(colRemoved) {
+    //const colNumberToRemove = this.colFile2.indexOf(colRemoved) + 1;
+    this.groupColumnFile2 = this.groupColumnFile2.filter(item => item !== colRemoved)
+  }
+  checkBoxEvent(data) {
     this.isGroup = data.target.checked;
     this.dropdownAction();
   }
-  dropdownAction(){
-    return {locked:!this.isGroup}
+  dropdownAction() {
+    return { locked: !this.isGroup }
+  }
+
+  printJSON() {
+    console.log('file array', this.fileArray);
+    console.log('keyColumnsFile1', this.keyColumnsFile1);
+    console.log('keyColumnsFile2', this.keyColumnsFile2);
+    console.log('is group', this.isGroup);
+    console.log('groupColumnFile1', this.groupColumnFile1);
+    console.log('groupColumnFile2', this.groupColumnFile2);
+
   }
 
 }
--- src/app/events/group/group.checkbox.component.ts
+++ src/app/events/group/group.checkbox.component.ts
@@ -1,17 +1,25 @@
-import { Component, Output, EventEmitter} from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 
 @Component({
-selector: 'check-box',
-template: `
-<input id = "groupcolumns" type="checkbox" (change)="isGroupSelected($event)"> Is Group?
+    selector: 'check-box',
+    template: `
+<input id = "groupcolumns" type="checkbox" (change)="isGroupSelected($event)"
+> Is Group?
+<img title="By default, output results are grouped at key columns. Select 'Is Group' to add any other column
+ in addition to the key column for grouping.For example if invoice no. is a key column then all value columns
+  would be clubbed at invoice level.If grouping is required at invoice and rate level, then rate column can be
+captured here"
+			src="http://pwcnic-2100337383.ap-south-1.elb.amazonaws.com/revreco-app2/assets/images/info2.png"
+			style="position:absolute;top:40px;right: 170px;">
+
 `
 })
 
 export class GroupCheckBox {
-@Output() checkBoxChecked = new EventEmitter();
+    @Output() checkBoxChecked = new EventEmitter();
 
-isGroupSelected(event){
-    this.checkBoxChecked.emit(event);
-}
+    isGroupSelected(event) {
+        this.checkBoxChecked.emit(event);
+    }
 }
